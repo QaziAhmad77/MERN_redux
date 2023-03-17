6cadffcbdf1d925396886ab56855aab44837483d
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-03-18 00:10:28 +0500

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-import reduxThunk from "redux-thunk";
-import rootReducer from "./root-reducer";
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import reduxThunk from 'redux-thunk';
+import rootReducer from './root-reducer';
 
 const middlewares = [reduxThunk];
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewares)
+);
 
 export default store;
