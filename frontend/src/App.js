74fb38393b7b1267e403c70dee0d28e2cb0df4ff
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-05-12 13:17:25 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
-import Router from "./pages/Router";
+import './App.css';
+import Router from './pages/Router';
 
 function App() {
   return (
@@ -10,3 +10,63 @@ function App() {
 }
 
 export default App;
+
+import React, { useState } from 'react';
+
+// const App = () => {
+//   const [nameList, setNameList] = useState(['Ahmad', 'Anas', 'Noman']);
+//   const [val, setVal] = useState('');
+//   const [id, setId] = useState('');
+//   const handleOnChange = (e) => {
+//     e.preventDefault();
+//     if (id === '') {
+//       setNameList([...nameList, val]);
+//       setVal('');
+//     }
+//     if (id !== '') {
+//       const newArr = [...nameList];
+//       newArr[id] = val;
+//       setNameList(newArr);
+//       setId('');
+//       setVal('');
+//     }
+//   };
+//   const delteName = (index) => {
+//     const filterArr = nameList.filter((item, i) => {
+//       return index !== i;
+//     });
+//     setNameList(filterArr);
+//   };
+//   const editName = (index) => {
+//     setVal(nameList[index]);
+//     setId(index);
+//   };
+//   return (
+//     <div>
+//       <h1>CRUP OPERATIONS</h1>
+//       <form action="" onSubmit={handleOnChange}>
+//         <input
+//           type="text"
+//           value={val}
+//           onChange={(e) => {
+//             setVal(e.target.value);
+//             console.log(e.target.value);
+//           }}
+//         />
+//         <button type="submit">ADD TO DO</button>
+//       </form>
+//       {nameList.map((item, index) => {
+//         return (
+//           <ul key={index}>
+//             <li>
+//               {item} <button onClick={(e) => editName(index)}>edit</button>
+//               <button onClick={(e) => delteName(index)}>delete</button>
+//             </li>
+//           </ul>
+//         );
+//       })}
+//     </div>
+//   );
+// };
+
+// export default App;
